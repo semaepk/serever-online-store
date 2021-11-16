f45b939cb0f3c6962a6afe6517cccfa4159e6758
semaepk <semaep.k@gmail.com>
2021-11-17 00:46:26 +0300

    update

--- routes/deviceRouter.js
+++ routes/deviceRouter.js
@@ -3,7 +3,7 @@ const router = new Router()
 const deviceController = require('../controllers/deviceController')
 const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', deviceController.create)
+router.post('/',checkRole('ADMIN'), deviceController.create)
 router.get('/', deviceController.getAll)
 router.get('/:id', deviceController.getOne)
 router.put('/:id', checkRole('ADMIN'), deviceController.update)
